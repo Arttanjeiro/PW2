b99d513e87578cd1c61cadce423f92c1b6572c20
Arthur Sousa <102182959+Arttanjeiro@users.noreply.github.com>
2023-02-02 17:33:49 -0300

    Começando os projetos

---
+++ aula01/src/main/java/edu/ifto/com/aula01/Aula01Application.java
@@ -0,0 +1,19 @@
+package edu.ifto.com.aula01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+
+public class Aula01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Aula01Application.class, args);
+
+		
+	
+
+	
+	}
+
+}
---
+++ aula01/src/main/java/edu/ifto/com/aula01/Controller/Controller.java
@@ -0,0 +1,15 @@
+package edu.ifto.com.aula01.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+  @GetMapping("/hello")
+  public String hello(@RequestParam(value = "name" , defaultValue = "Word") String name) {
+        return String.format("Hello %s!", name);
+  }
+  
+}
